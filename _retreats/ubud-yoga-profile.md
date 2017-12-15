b35a1746136ad1ed61ffed1d767511b96cd999f4
Cassie <cassie.balista@hotmail.com>
2017-12-15 19:08:07 +0800

    draft-8

--- _retreats/ubud-yoga-profile.md
+++ _retreats/ubud-yoga-profile.md
@@ -57,4 +57,8 @@ background: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiu
 back-url: www.google.com
 exclusions: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 faq: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+gallery:
+  - image_path: /images/bali-field.jpg
+  - image_path: /images/bondiice.jpg
+
 ---
--- _site/main.js
+++ _site/main.js
@@ -12,7 +12,7 @@ $(".dropdown-nav").click(function(){
     $(this).children(".dropdown-content-mob").slideDown("200");
   });
 
-  // write jquery below here
+  // This is your smooth scrolling
 
     // Select all links with hashes
     $('a[href*="#"]')
@@ -50,6 +50,35 @@ $(".dropdown-nav").click(function(){
           }
         }
       });
+
+
+
+
+
+
+    // this is the jquery to initalise the gallery popup
+
+    $('.popup-gallery').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  tLoading: 'Loading image #%curr%...',
+  mainClass: 'mfp-img-mobile',
+  gallery: {
+    enabled: true,
+    navigateByImgClick: true,
+    preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  },
+  image: {
+    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    titleSrc: function(item) {
+      return item.el.attr('title');
+    }
+  }
+});
+
+
+
+
 
 // post jquery above here
 
